285abcd30348f5f8e97300d96ed145ce41734edc
Alan Berman <thealanberman@gmail.com>
2018-07-26 09:25:53 -0700

    functionalize all the things

--- setup.sh
+++ setup.sh
@@ -1,21 +1,41 @@
 #!/usr/bin/env bash
 
 # make sure we have git
-[[ -f "/usr/bin/git" ]] && { echo "git found. moving on..."; }|| { echo "git not installed."; exit; }
+[[ $(command -v git) ]] && { install_bashit; } || { echo "git not installed."; }
+# TODO 
+# fix this case statement to handle Linux or Darwin
+# case foo in $(uname)
+#   "Darwin")
+#     install_homebrew
+#     install_brew_apps
+#   *)
+# fi
 
-read -e -s -p "Install Bash-it [y/N]? " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  /usr/bin/git clone --depth=1 https://github.com/Bash-it/bash-it.git ${HOME}/.bash-it
-  eval "${HOME}/.bash-it/install.sh --silent --no-modify-config"
-fi
 
-if [[ $(uname) == "Darwin" ]]; then
+install_bashit() 
+{
+  read -e -s -p "Install Bash-it [y/N]? " -n 1 -r
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    /usr/bin/git clone --depth=1 https://github.com/Bash-it/bash-it.git ${HOME}/.bash-it
+    eval "${HOME}/.bash-it/install.sh --silent --no-modify-config"
+  fi
+}
 
+install_git()
+{
+  sudo apt-get update && sudo apt-get install git
+}
+
+install_homebrew()
+{
   read -e -s -p "Install Homebrew [y/N]? " -n 1 -r
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
+}
 
+install_brew_apps()
+{
   read -e -s -p "Install Homebrew apps [y/N]? " -n 1 -r
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     brew install \
@@ -41,8 +61,4 @@ if [[ $(uname) == "Darwin" ]]; then
     vim \
     wget 
   fi
-
-fi
-
-echo "Be sure to 'source ~/.bash_profile' to have your settings take effect."
-echo "Then './configure-bash-it.sh' to customize your shell."
+}
