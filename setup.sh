cc04b21bb5d82ba5d4bf73c2678a9b92e8dc7042
Alan Berman <thealanberman@gmail.com>
2021-07-25 20:21:40 -0700

    fix: don't exit on errors

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Exit on error. Append "|| true" if you expect an error.
-set -o errexit # set -e
+# set -o errexit # set -e
 # Exit on error inside any functions or subshells.
-set -o errtrace # set -E
+# set -o errtrace # set -E
 # Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
 set -o nounset # set -u
 # Catch the error in case 1st command fails but piped command succeeds
